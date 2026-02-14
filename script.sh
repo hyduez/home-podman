40524e992ce6c3071eb46d84996e13b0ced79ca3
Paulo Viteri <undevoted@proton.me>
2026-02-14 00:01:49 -0500

    chore: some updates, i removed my gpu

--- script.sh
+++ script.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/bash
+set -e
 
-for d in */; do [ -d "$d" ] && cd "$d" && podman-compose up -d && cd ..; done
+LOCK_FILE="$HOME/.pods_service_lock"
+
+if [ -f "$LOCK_FILE" ]; then
+  echo "Pods service already running, exiting"
+  exit 0
+fi
+
+touch "$LOCK_FILE"
+
+trap 'rm -f "$LOCK_FILE"' EXIT
+
+for d in $HOME/Workspaces/*/; do
+  [ -d "$d" ] && (
+    cd "$d" || exit
+    if ! podman ps --format '{{.Names}}' | grep -q -x "$(basename "$d")"; then
+    	podman-compose up -d
+    	sleep 10
+    fi
+  )
+done
